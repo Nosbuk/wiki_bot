f8cb4257a762354a95bbf32ead4c9e34204a83ba
JM <jakub.matwis@gmail.com>
2022-12-15 13:14:35 -0800

    feat: chatBot webHook controller

---
+++ src/controllers/chatBotController.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const request = require("request");
+const localToken = process.env.META_TOKEN
+const postWebhook = (req, res) => {
+  // Parse the request body from the POST
+  const body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === "page") {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function (entry) {
+      // Gets the body of the webhook event
+      const webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send("EVENT_RECEIVED");
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+};
+
+const getWebhook = (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === localToken) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+};
+
+module.exports = { postWebhook };
--- src/routes/web.js
+++ src/routes/web.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const initWebRoutes = (app) => {
     router.get("/", homePageControler.getHomepage)
+    
     return app.use("/", router)
 }
 
