b56017fc77a6b4567d0d5bbae908b2e049171dfa
JM <jakub.matwis@gmail.com>
2022-12-15 10:58:33 -0800

    feat: created view & view controller for homepage route

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,9 @@
 const express = require( "express");
-
+const homePageControler = require("../controllers/homepageController")
 const router = express.Router();
 
 const initWebRoutes = (app) => {
-    router.get("/", (req, res)=> {
-        return res.send("Hello world!")
-    })
+    router.get("/", homePageControler.getHomepage)
     return app.use("/", router)
 }
 
